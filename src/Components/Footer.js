23d85d0531b8200589e3d4d8ca6f529c5abf8ceb
wizmag <wizblast@yandex.ru>
2022-12-29 17:07:10 +0400

    edit footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+const company_item = ['About', 'Prtners', 'Requisites'];
+const contacts_item = [
+  '52 Forest Green, South palm 435',
+  '+1 234 56 78',
+  'wizz-support@gmail.com',
+];
 
 const Footer = () => {
   return (
-    <div className='h-full flex justify-between'>
-      <section className='flex-1'>
-        <div>Company</div>
-        <div>About</div>
-        <div>Partners</div>
-        <div>Requisites</div>
+    <div className='h-full flex justify-between px-10 py-6'>
+      <section className='flex-1 flex justify-center'>
+        <div>
+          <div className='font-bold mb-3 italic'>Company</div>
+          {company_item.map((item) => (
+            <ul>
+              <li className='cursor-pointer hover:underline'>{item}</li>
+            </ul>
+          ))}
+        </div>
       </section>
-      <section className='flex-1'>
-        <div>Contacts</div>
-        <div>Telephone</div>
-        <div>Adress</div>
-        <div>Email</div>
+      <section className='flex-1 flex justify-center'>
+        <div>
+          <div className='font-bold mb-3 italic'>Contacts</div>
+          {contacts_item.map((item) => (
+            <ul>
+              <li className='cursor-pointer hover:underline'>{item}</li>
+            </ul>
+          ))}
+        </div>
       </section>
-      <section className='flex-1'>
-        <div>Connected</div>
-        <div>Twitter</div>
-        <div>Instagramm</div>
-        <div>Facebook</div>
+      <section className='flex-1 flex justify-center'>
+        <div>
+          <div className='font-bold mb-3 italic'>Connected</div>
+          <div className='flex gap-8'>
+            <div className='h-10 w-10 bg-blue-600 rounded-[50%] text-white flex justify-center items-center cursor-pointer'>
+              <TwitterIcon />
+            </div>
+            <div className='h-10 w-10 bg-amber-800 rounded-[50%] text-white flex justify-center items-center cursor-pointer'>
+              <InstagramIcon />
+            </div>
+            <div className='h-10 w-10 bg-blue-900 rounded-[50%] text-white flex justify-center items-center cursor-pointer'>
+              <FacebookIcon />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
