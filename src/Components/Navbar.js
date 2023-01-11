317fb65d6bd79b8b903d40cf77e9009f4aec6dab
wizmag <wizblast@yandex.ru>
2023-01-11 23:59:08 +0400

    add useNavigate

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const types = ['Overcoat', 'Blouse', 'Jeans', 'Shorts', 'Other'];
+const types = [
+  { name: 'Overcoat', navigation: '/overcoat' },
+  { name: 'Blouse', navigation: '/blouse' },
+  {
+    name: 'Jeans',
+    navigation: '/jeans',
+  },
+  {
+    name: 'Shorts',
+    navigation: '/shorts',
+  },
+  {
+    name: 'Other',
+    navigation: '/other',
+  },
+];
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='flex justify-center items-center gap-12 h-12 w-full px-32'>
       {types.map((item) => (
-        <p className='font-bold hover:underline cursor-pointer' key={item}>
-          {item}
+        <p
+          className='font-bold hover:underline cursor-pointer'
+          key={item.name}
+          onClick={() => navigate(item.navigation)}
+        >
+          {item.name}
         </p>
       ))}
     </div>
